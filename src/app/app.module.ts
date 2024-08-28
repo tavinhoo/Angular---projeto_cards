2a9c13d1e5ccd693a2faf4eaa23b8cb594cfec37
Luiz Otávio <luizotaviomtbs@gmail.com>
2024-08-28 18:55:36 -0300

    Cards criados - Projeto base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardRoxoComponent } from './card-roxo/card-roxo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardRoxoComponent
   ],
   imports: [
     BrowserModule,
