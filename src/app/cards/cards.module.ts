7ff23a3d95a7d3ff9d79c2ed604f231b77369afb
Luiz Otávio <luizotaviomtbs@gmail.com>
2024-08-30 17:22:17 -0300

    Importanto estilos

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -4,20 +4,22 @@ import { CardButtonComponent } from '../card-button/card-button.component';
 import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
 import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
 import { CardComponent } from '../card/card.component';
+import { CardButtonCancelComponent } from '../card-button-cancel/card-button-cancel.component';
 
 @NgModule({
   declarations: [
     CardComponent,
     CardRoxoComponent,
     CardButtonComponent,
-    CardRoxoButtonComponent
+    CardRoxoButtonComponent,
+    CardButtonCancelComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     CardComponent,
-    CardRoxoComponent
+    CardRoxoComponent,
   ]
 })
 export class CardsModule { }
