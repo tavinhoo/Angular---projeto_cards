ef80b859fb8ef5b598423e82639cfb69efb9cf8e
Luiz Otávio <luizotaviomtbs@gmail.com>
2024-09-04 11:39:56 -0300

    View Encapsulation Shadow Dom

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -13,7 +13,7 @@ interface IInfos {
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
-  encapsulation: ViewEncapsulation.Emulated
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 
 export class CardComponent {
