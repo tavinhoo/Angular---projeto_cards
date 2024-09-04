ba6e42f05d5a6e5a044f676b43b1087f269c5e02
Luiz Otávio <luizotaviomtbs@gmail.com>
2024-09-04 11:31:43 -0300

    View Encapsulation Emulated

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -13,7 +13,7 @@ interface IInfos {
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.Emulated
 })
 
 export class CardComponent {
